740e23be7397dd5b3d4251b62beb0e88a49656fc
perljustine <justine.perl@hotmail.com>
2024-11-10 22:26:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// var openBtn = document.getElementById("openBtn");
+// var closeBtn = document.getElementById("closeBtn");
+
+function ToggleBurger() {
+  var burger = document.getElementById("Burger");
+  var menu = document.getElementById("Menu");
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+}
